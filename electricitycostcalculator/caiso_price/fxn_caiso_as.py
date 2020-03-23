d60df9ef08d4aadc219f75a727c150582be4bdc9
Adya Indera <adyaindera@berkeley.edu>
2020-03-23 12:36:26 -0700

    Created functions and extract csv files

--- electricitycostcalculator/caiso_price/fxn_caiso_as.py
+++ electricitycostcalculator/caiso_price/fxn_caiso_as.py
@@ -3,66 +3,84 @@
 import datetime
 import pytz
 import requests
+import pandas as pd
+from zipfile import ZipFile
 
 # fn_name(start_date, end_date, node_list, tz_name)
 # output: a pandas DataFrame, 2 columns per node, one for RTP and one for DAM
-def get_CAISO_as(start_date=datetime.date.today(), 
-                  end_date=datetime.date.today(), 
+def get_CAISO_as(start_date=datetime.date(2019, 1, 1), 
+                  end_date=datetime.date(2019, 1, 2), 
                   market_run_id='DAM', 
-                  nodes=['DLAP_PGAE-APND', 'DLAP_SCE-APND', 'DLAP_SDGE-APND'],
+                  nodes=['DLAP_PGAE-APND'],
                   tz_name='UTC', 
-                  raw_dir = '.'): 
+                  raw_dir = '.'):
 
-# DEFAULT TIME ZONE: UTC, use pytz
-  # 
-  #  this function simply grabs the csv data from caiso and dumps it into a data frame with the same structure as the csv.
-  # `startdate` and `enddate` have format yyyymmdd (strings). This function doesn't work for very old data. Known to be OK for 2017.
-  # `market_run_id` is either "DAM" or "RTM" for day ahead or real time markets
-  # `nodes` is a list of APNODES to get. The default options correspond to the PGE, SCE, and SDGE DLAP (aggregated pricing nodes)
-  # `raw_dir` is a location to store unzipped .csv files. There is currently no option to automatically delete these. 
-  
-  #define base URL
-  baseURL = 'http://oasis.caiso.com/oasisapi/SingleZip?'
+    # DEFAULT TIME ZONE: UTC, use pytz
+    # 
+    #  this function simply grabs the csv data from caiso and dumps it into a data frame with the same structure as the csv.
+    # `startdate` and `enddate` have format yyyymmdd (strings). This function doesn't work for very old data. Known to be OK for 2017.
+    # `market_run_id` is either "DAM" or "RTM" for day ahead or real time markets
+    # `nodes` is a list of APNODES to get. The default options correspond to the PGE, SCE, and SDGE DLAP (aggregated pricing nodes)
+    # `raw_dir` is a location to store unzipped .csv files. There is currently no option to automatically delete these. 
 
-  if market_run_id == 'RTM':
-    market_query = 'queryname=PRC_INTVL_LMP&resultformat=6&version=1&market_run_id=RTM'
+    if not isinstance(nodes, list):
+        nodes = [nodes]
 
-  if market_run_id == 'DAM':
-    market_query = 'queryname=PRC_LMP&resultformat=6&version=1&market_run_id=DAM'
+    dataframe = None
+    for node in nodes:
+        url = get_api_url(start_date, end_date, market_run_id, node, tz_name)
 
-  try:
-    timezone = pytz.timezone(tz_name)
-  except:
-    timezone = pytz.utc
+        filename = download_and_extract_csv(url)
 
-  start_date = start_date.strftime("%Y%m%d") + "T08:00-0000"
-  end_date = end_date.strftime("%Y%m%d") + "T08:00-0000"
+        df = pd.read_csv(filename)
 
-  datetime_query = "&startdatetime=" + start_date + "&enddatetim=" + end_date
+# Return the filename of the extracted csv file
+def download_and_extract_csv(url):
+    print("Getting url: ", url)
+    caiso_data = requests.get(url, stream=False)
 
-  for node in nodes:
+    filename = caiso_data.headers['Content-Disposition'][17:-1]
+    print("Downloaded filename: ", filename)
+    with open(filename, 'wb') as f:
+        f.write(caiso_data.content)
+
+    with ZipFile(filename, 'r') as zipObj:
+        file_csv = zipObj.namelist()[0]
+        print("Downloaded csv filename: ", file_csv)
+        zipObj.extract(file_csv)
+
+    return file_csv
+    
+
+def get_api_url(start_date, end_date, market_run_id, node, tz_name):
+    #define base URL
+    baseURL = 'http://oasis.caiso.com/oasisapi/SingleZip?'
+
+    if market_run_id == 'RTM':
+      market_query = 'queryname=PRC_INTVL_LMP&resultformat=6&version=1&market_run_id=RTM'
+
+    if market_run_id == 'DAM':
+      market_query = 'queryname=PRC_LMP&resultformat=6&version=1&market_run_id=DAM'
+
+    try:
+      timezone = pytz.timezone(tz_name)
+    except:
+      timezone = pytz.utc
+
+    duration = end_date - start_date
+
+    if duration.days > 31 or duration.days < 1:
+      print("ERROR: duration must be at least 1 day and at most 31 days")
+      return
+
+    start_date = start_date.strftime("%Y%m%d") + "T08:00-0000"
+    end_date = end_date.strftime("%Y%m%d") + "T08:00-0000"
+
+    datetime_query = "&startdatetime=" + start_date + "&enddatetime=" + end_date
     node_query = "&node=" + node
+
     url = baseURL + market_query + datetime_query + node_query
 
-    caiso_data = requests.get(url)
-   
-  
-  # for(i in 1:nrow(get_options)){
-    
-  # getURL <- paste0(get_options[i,base], get_options[i,market], get_options[i,date], get_options[i,node])
-    
-  
-  # temp <- tempfile() #create temp file
-  # try(download.file(getURL,temp)) # get data into temp
-  # try(newdata <- read.csv(unzip(temp, exdir = raw_dir))) # unzip and read csv, dump into data
-  # unlink(temp)
-  
-  # if(i == 1){data <- newdata}
-  # if(i > 1){ try(data <- rbind(data,newdata))}#append new data to existing
-  
-  #   Sys.sleep(5.1) # wait 5.1 seconds so CAISO doesn't send HTTP error
-  # }
- 
-  # return(as.data.table(data))
-# }
-    
+    return url
+
+# get_CAISO_as()
