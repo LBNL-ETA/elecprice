8f92618314fc78b14188077d8b39b54133557ed0
Adya Indera <adyaindera@berkeley.edu>
2020-04-09 12:14:38 -0700

    Added function to get LMP price

--- electricitycostcalculator/caiso_price/fxn_caiso_as.py
+++ electricitycostcalculator/caiso_price/fxn_caiso_as.py
@@ -1,86 +1,111 @@
 # fetch CAISO LMP data from OASIS (see "Interface Specification for OASIS API)
 
 import datetime
+import time
 import pytz
 import requests
 import pandas as pd
+import os
 from zipfile import ZipFile
 
-# fn_name(start_date, end_date, node_list, tz_name)
-# output: a pandas DataFrame, 2 columns per node, one for RTP and one for DAM
-def get_CAISO_as(start_date=datetime.date(2019, 1, 1), 
-                  end_date=datetime.date(2019, 1, 2), 
-                  market_run_id='DAM', 
-                  nodes=['DLAP_PGAE-APND'],
-                  tz_name='UTC', 
-                  raw_dir = '.'):
-
-    # DEFAULT TIME ZONE: UTC, use pytz
-    # 
-    #  this function simply grabs the csv data from caiso and dumps it into a data frame with the same structure as the csv.
-    # `startdate` and `enddate` have format yyyymmdd (strings). This function doesn't work for very old data. Known to be OK for 2017.
-    # `market_run_id` is either "DAM" or "RTM" for day ahead or real time markets
-    # `nodes` is a list of APNODES to get. The default options correspond to the PGE, SCE, and SDGE DLAP (aggregated pricing nodes)
-    # `raw_dir` is a location to store unzipped .csv files. There is currently no option to automatically delete these. 
+# Return Pandas dataframe with end time, nodes, and LMP price aggregration
+# NODES could be single string or a list.
+def get_caiso_price(start_date=datetime.datetime(2019, 1, 1, 8, 0, 0),
+                     end_date=datetime.datetime(2019, 1, 2, 8, 0, 0),
+                     market_run_id='RTM', 
+                     nodes=['DLAP_PGAE-APND', 'DLAP_SCE-APND'],
+                     tz_name='UTC'):
 
     if not isinstance(nodes, list):
         nodes = [nodes]
 
-    dataframe = None
+    dataframes = None
     for node in nodes:
-        url = get_api_url(start_date, end_date, market_run_id, node, tz_name)
+        df = get_CAISO_lmp(start_date, end_date, market_run_id, node, tz_name)
+        if dataframes is not None:
+            dataframes = dataframes.append(df)
+        else:
+            dataframes = df
 
-        filename = download_and_extract_csv(url)
+    dataframes = dataframes[['INTERVALENDTIME_GMT', 'XML_DATA_ITEM', 'PNODE_RESMRID', 'MW']]
 
-        df = pd.read_csv(filename)
+    summary_filename = "caiso_LMP_" + str(start_date.year) + "_" + market_run_id + "_" + nodes[0][:4] \
+                              + "_interval_ending_summary.csv"
+    
+    summary_df = pd.pivot_table(dataframes, values='MW', index=['INTERVALENDTIME_GMT', 'PNODE_RESMRID'], columns='XML_DATA_ITEM', aggfunc=sum)
+    summary_df.to_csv(summary_filename)
 
-# Return the filename of the extracted csv file
+    return summary_df
+
+# Return Pandas dataframe of CAISO data according to the parameters.
+def get_CAISO_lmp(start_date=datetime.datetime(2019, 1, 1, 8, 0, 0),
+                  end_date=datetime.datetime(2019, 1, 2, 8, 0, 0),
+                  market_run_id='DAM', 
+                  node='DLAP_SCE-APND',
+                  tz_name='UTC'):
+
+    url = get_api_url(start_date, end_date, market_run_id, node, tz_name)
+
+    start = start_date.strftime("%Y%m%d")
+    end = end_date.strftime("%Y%m%d")
+
+    filename = "caiso_lmp_node_" + node + "_" + market_run_id + "_" + start + "_" + end + ".csv"
+    old_filename = download_and_extract_csv(url)
+    os.rename(old_filename, filename)
+    
+    time.sleep(5)
+
+    return pd.read_csv(filename)
+
+# Download ZIP from URL and extract the CSV file, return the CSV file name.
 def download_and_extract_csv(url):
     print("Getting url: ", url)
     caiso_data = requests.get(url, stream=False)
 
     filename = caiso_data.headers['Content-Disposition'][17:-1]
-    print("Downloaded filename: ", filename)
+    print("Downloaded zip: ", filename)
     with open(filename, 'wb') as f:
         f.write(caiso_data.content)
 
     with ZipFile(filename, 'r') as zipObj:
         file_csv = zipObj.namelist()[0]
-        print("Downloaded csv filename: ", file_csv)
+        print("Downloaded csv: ", file_csv, "\n")
         zipObj.extract(file_csv)
 
     return file_csv
-    
 
+# Return CAISO API URL according to the parameters.
 def get_api_url(start_date, end_date, market_run_id, node, tz_name):
     #define base URL
     baseURL = 'http://oasis.caiso.com/oasisapi/SingleZip?'
-
-    if market_run_id == 'RTM':
-      market_query = 'queryname=PRC_INTVL_LMP&resultformat=6&version=1&market_run_id=RTM'
-
-    if market_run_id == 'DAM':
-      market_query = 'queryname=PRC_LMP&resultformat=6&version=1&market_run_id=DAM'
-
-    try:
-      timezone = pytz.timezone(tz_name)
-    except:
-      timezone = pytz.utc
-
     duration = end_date - start_date
 
     if duration.days > 31 or duration.days < 1:
-      print("ERROR: duration must be at least 1 day and at most 31 days")
-      return
+        print("ERROR: duration must be at least 1 day and at most 31 days")
+        return
 
-    start_date = start_date.strftime("%Y%m%d") + "T08:00-0000"
-    end_date = end_date.strftime("%Y%m%d") + "T08:00-0000"
+    if market_run_id == 'RTM':
+        market_query = 'queryname=PRC_INTVL_LMP&resultformat=6&version=1&market_run_id=RTM'
 
-    datetime_query = "&startdatetime=" + start_date + "&enddatetime=" + end_date
+    if market_run_id == 'DAM':
+        market_query = 'queryname=PRC_LMP&resultformat=6&version=1&market_run_id=DAM'
+
+    timezone = pytz.timezone(tz_name)
+
+    # Set timezone
+    start_date = timezone.localize(start_date)
+    end_date = timezone.localize(end_date)
+
+    # Convert date to the desired timezone
+    utc_start_date = start_date.astimezone(pytz.utc)
+    utc_end_date = end_date.astimezone(pytz.utc)
+
+    utc_start_date = utc_start_date.strftime("%Y%m%dT%H:%M-0000")
+    utc_end_date = utc_end_date.strftime("%Y%m%dT%H:%M-0000")
+
+    datetime_query = "&startdatetime=" + utc_start_date + "&enddatetime=" + utc_end_date
     node_query = "&node=" + node
 
     url = baseURL + market_query + datetime_query + node_query
 
     return url
-
-# get_CAISO_as()
